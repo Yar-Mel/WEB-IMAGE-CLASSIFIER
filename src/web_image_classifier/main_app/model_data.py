65d1c191526a3fe3e74d827d18396467e2192a74
Yaroslav Melnychuk <126187631+Yar-Mel@users.noreply.github.com>
2024-02-13 13:06:21 +0200

    Add alpha version of frontend (#1)

---
+++ src/web_image_classifier/main_app/model_data.py
@@ -0,0 +1,12 @@
+CLASSES_LIST = [
+    "airplane",
+    "automobile",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+]
--- src/web_image_classifier/web_image_classifier/settings.py
+++ src/web_image_classifier/web_image_classifier/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +79,10 @@ WSGI_APPLICATION = 'web_image_classifier.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
