42fe302fd731605646ae27d897ad618af4c86dfc
Noma9d <woody0740@gmail.com>
2024-02-17 14:58:54 +0200

    изменено:      src/web_image_classifier/main_app/utils.py

--- src/web_image_classifier/main_app/utils.py
+++ src/web_image_classifier/main_app/utils.py
@@ -4,107 +4,127 @@ import tensorflow as tf
 from django.http import HttpResponseBadRequest
 from . import tf_models as models
 import cv2
+from typing import Tuple
 
 
 
 class UploadProcessing:
-    def __init__(self, image_file, allowed_extensions, max_size):
-        self.image_file = image_file
+    def __init__(self, allowed_extensions=('.jpg', '.jpeg', '.SVG'), max_size=50):
+        self._image_file = None
         self.allowed_extensions = allowed_extensions
         self.max_size = max_size
 
+    @property
+    def image_file(self):
+        return self._image_file
 
-    def validate_image(image_file, allowed_extensions=('.jpg', '.jpeg'), max_size=50 ):
+    @image_file.setter
+    def image_file(self, value):
+        self._validate_image(value)
+        self._image_file = value
+
+    def _validate_image(self, image_file):
         """
         Проверяет расширение и размер изображения.
 
         Parameters:
         - image_file (InMemoryUploadedFile): Файл изображения, полученный из запроса.
-        - allowed_extensions (tuple): Кортеж разрешенных расширений изображения.
-        - max_size (int): Максимальный размер изображения.
-
-        Returns:
-        - image_file (InMemoryUploadedFile): Файл изображения, если он валиден.
-
-        Raises:
-        - HttpResponseBadRequest: Если расширение изображения неверное или размер изображения слишком большой.
         """
         # Проверяем расширение файла
-        if not image_file.name.lower().endswith(allowed_extensions):
+        if not image_file.name.lower().endswith(self.allowed_extensions):
             raise HttpResponseBadRequest(
-                f"Неверное расширение изображения. Пожалуйста, загрузите файлы с расширением {', '.join(allowed_extensions)}.")
+                f"Неверное расширение изображения. Пожалуйста, загрузите файлы с расширением {', '.join(self.allowed_extensions)}.")
 
-        
         # Проверяем размер файла
-        max_size = max_size * 1024 * 1024
-        if image_file.size > max_size:
+        max_size_bytes = self.max_size * 1024 * 1024
+        if image_file.size > max_size_bytes:
             raise HttpResponseBadRequest(
-                f"Слишком большой размер изображения. Максимальный размер - {max_size / (1024 * 1024)} MB.")
-
-        # Если изображение прошло валидацию, возвращаем его
-        return image_file
+                f"Слишком большой размер изображения. Максимальный размер - {self.max_size} MB.")
 
 
 class Classification:
-    def __init__(self, model, image_file):
+    """
+    Класс для классификации изображений с использованием модели TensorFlow.
+
+    Attributes:
+    - model (str): Путь к файлу модели TensorFlow в формате .h5.
+    - image_file (InMemoryUploadedFile): Файл изображения, полученный из запроса.
+
+    Methods:
+    - __call__(*args, **kwargs): Метод для вызова класса как функции.
+    - process_image_raster(image_file: str, target_size: Tuple[int, int] = (32, 32)) -> np.ndarray:
+        Обрабатывает растровое изображение.
+    - process_image_vector(vector_image: str, target_size: Tuple[int, int] = (32, 32)) -> np.ndarray:
+        Преобразует векторное изображение.
+    - get_prediction(image_array: np.ndarray, model: str) -> dict:
+        Получает предсказание модели на основе изображения.
+    """
+
+    def __init__(self, model: str, image_file):
+        """
+        Инициализация класса.
+
+        Parameters:
+        - model (str): Путь к файлу модели TensorFlow в формате .h5.
+        - image_file (InMemoryUploadedFile): Файл изображения, полученный из запроса.
+        """
         self.model = model
         self.image_file = image_file
 
-    def process_image_raster(self, image_file, target_size=(32, 32)):
+    ALLOWED_EXTENSION = {
+        'rastr': ('.jpg', '.jpeg'),
+        'vector': ('SVG',)
+    }
+
+    def __call__(self, *args, **kwargs):
         """
-        Обрабатывает загруженное изображение.
+        Метод для вызова класса как функции.
+        """
+        if self.image_file.lower().endswith(self.ALLOWED_EXTENSION['rastr']):
+            image_array = self.process_image_raster(self.image_file)
+            predictions = self.get_prediction(image_array, self.model)
+        elif self.image_file.lower().endswith(self.ALLOWED_EXTENSION['vector']):
+            image_array = self.process_image_raster(self.image_file)
+            predictions = self.get_prediction(image_array, self.model)
+        
+        return predictions
+
+    def process_image_raster(self, image_file: Image, target_size: Tuple[int, int] = (32, 32)) -> np.ndarray:
+        """
+        Обрабатывает загруженное растровое изображение.
 
         Parameters:
-        - image_file (InMemoryUploadedFile): Файл изображения, полученный из запроса
+        - image_file (InMemoryUploadedFile): Файл изображения, полученный из запроса.
         - target_size (tuple): Целевой размер изображения после преобразования. По умолчанию (32, 32).
 
         Returns:
         - image_array (numpy.ndarray): Массив, представляющий изображение.
         """
-
-        # Читаем изображение в память
         image = Image.open(image_file)
-
-        # Изменяем размер изображения до целевого размера
         image = image.resize(target_size)
-
-        # Переводим изображение в формат numpy массива
-        image_array = np.array(image) / 255.0  # Нормализация значений пикселей
-
+        image_array = np.array(image) / 255.0
         return image_array
-    
 
-    def process_image_for_tensorflow(self, vector_image, target_size=(32, 32)):
+    def process_image_vector(self, vector_image: Image, target_size: Tuple[int, int] = (32, 32)) -> np.ndarray:
         """
         Преобразует векторное изображение в формат, подходящий для модели TensorFlow.
 
         Parameters:
-        - vector_image (InMemoryUploadedFile): Файл изображения, полученный из запроса
+        - image_file (InMemoryUploadedFile): Файл изображения, полученный из запроса.
         - target_size (tuple): Целевой размер изображения после преобразования. По умолчанию (32, 32).
 
         Returns:
         - processed_image (numpy.ndarray): Изображение в формате, подходящем для модели TensorFlow.
         """
-
-        # Загрузка векторного изображения (SVG, например)
-        # и преобразование его в растровое изображение (PNG, например)
         vector_image = cv2.imread(vector_image)
-
-        # Изменение размера изображения до целевого размера
         resized_image = cv2.resize(vector_image, target_size)
-
-        # Нормализация значений пикселей
         normalized_image = resized_image / 255.0
-
-        # Добавление размерности для соответствия ожидаемому формату входных данных модели TensorFlow
         processed_image = np.expand_dims(normalized_image, axis=0)
-
         return processed_image
 
-
-    def get_prediction(self, image_array, model):
+    def get_prediction(self, image_array: np.ndarray, model: str) -> dict:
         """
-        Получает предсказание модели на основе переданного изображения в виде массива и пути к файлу модели.
+        Получает предсказание модели на основе переданного изображения и пути к файлу модели.
 
         Parameters:
         - image_array (numpy.ndarray): Массив, представляющий изображение.
@@ -112,31 +132,16 @@ class Classification:
 
         Returns:
         - prediction (dict): Словарь с предсказанными классами изображения моделью.
-
-        Raises:
-        - None
         """
-        # Загружаем модель TensorFlow из файла .h5
         model = tf.keras.models.load_model(model)
-
-        # Выполняем предсказание
         prediction = model.predict(np.expand_dims(image_array, axis=0))
-
-        # Получаем три наиболее вероятных класса
-        top_classes = tf.argsort(prediction, axis=1, direction='DESCENDING')[:,:3]
-
-        # Получаем имена классов и вероятности
-        classes = ['class1', 'class2', 'class3']  # Поменяйте на реальные имена классов
+        top_classes = tf.argsort(prediction, axis=1, direction='DESCENDING')[:, :3]
+        classes = ['class1', 'class2', 'class3']  # Замените на реальные имена классов
         probabilities = prediction[0][top_classes[0]].numpy()
-
-        # Сохраняем предсказание в формате, как указано в вашем запросе
         prediction_dict = {
             'first': classes[top_classes[0][0]] + ': ' + str(round(probabilities[0] * 100, 2)) + '%',
             'second': classes[top_classes[0][1]] + ': ' + str(round(probabilities[1] * 100, 2)) + '%',
             'third': classes[top_classes[0][2]] + ': ' + str(round(probabilities[2] * 100, 2)) + '%',
         }
-
-        # Удаляем изображение из памяти
-        del image_array
 
         return prediction_dict
