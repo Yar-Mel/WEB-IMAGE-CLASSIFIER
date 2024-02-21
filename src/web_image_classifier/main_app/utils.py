815ad3670b421e82f2ec48f55a70c6efa4bdb2a0
Yaroslav Melnychuk <yarmel.dev@gmail.com>
2024-02-21 09:43:05 +0200

    branch-final-commit

--- src/web_image_classifier/main_app/utils.py
+++ src/web_image_classifier/main_app/utils.py
@@ -1,24 +1,29 @@
-from PIL import Image
 import numpy as np
 import tensorflow as tf
 from django.http import HttpResponseBadRequest
-import cv2
 from typing import Tuple
 from itertools import chain
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from copy import deepcopy
+from PIL import Image
+from io import BytesIO
+import cairosvg
+import cv2
 from .tf_models.models_info import CIFAR_10_LIST
 import cairosvg
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from io import BytesIO
 
-ALLOWED_EXTENSION = {
-        'rastr': ('jpg', 'jpeg', 'png'),
-        'vector': ('svg',)
-    }
+
+ALLOWED_EXTENSION = {"rastr": ("jpg", "jpeg", "png"), "vector": ("svg",)}
 
 
 class UploadProcessing:
-    def __init__(self, allowed_extensions=tuple(chain.from_iterable(ALLOWED_EXTENSION.values())), max_size=50):
+    def __init__(
+        self,
+        allowed_extensions=tuple(chain.from_iterable(ALLOWED_EXTENSION.values())),
+        max_size=50,
+    ):
         """
         Initializes the UploadProcessing object.
 
@@ -50,17 +55,28 @@ class UploadProcessing:
         self._image = deepcopy(Image.open(value))
 
     def _svg_to_png(self, image):
+<<<<<<< Updated upstream
         if not isinstance(image, InMemoryUploadedFile) or not image.name.lower().endswith('.svg'):
+=======
+        if not isinstance(
+            image, InMemoryUploadedFile
+        ) or not image.name.lower().endswith(".svg"):
+>>>>>>> Stashed changes
             return image  # If the file is not SVG or not InMemoryUploadedFile, return it as is
         else:
             svg_content = image.read()  # Read the content of the SVG file
             image.seek(0)  # Return the cursor to the beginning of the file
             png_content = cairosvg.svg2png(bytestring=svg_content)  # Convert SVG to PNG
+<<<<<<< Updated upstream
     
+=======
+
+>>>>>>> Stashed changes
             # Create a BytesIO object to save the PNG image in memory
             png_buffer = BytesIO()
             png_buffer.write(png_content)
             png_buffer.seek(0)
+<<<<<<< Updated upstream
     
             # Create an InMemoryUploadedFile object for the PNG image
             png_file = InMemoryUploadedFile(png_buffer, None, image.name.replace('.svg', '.png'), 'image/png', png_buffer.tell(), None)
@@ -68,6 +84,21 @@ class UploadProcessing:
             return png_file  # Return the PNG image
 
 
+=======
+
+            # Create an InMemoryUploadedFile object for the PNG image
+            png_file = InMemoryUploadedFile(
+                png_buffer,
+                None,
+                image.name.replace(".svg", ".png"),
+                "image/png",
+                png_buffer.tell(),
+                None,
+            )
+
+            return png_file  # Return the PNG image
+
+>>>>>>> Stashed changes
     def _validate_image(self, image):
         """
         Checks the extension and size of the image.
@@ -78,13 +109,15 @@ class UploadProcessing:
         # Check the file extension
         if not image.name.lower().endswith(self.allowed_extensions):
             raise HttpResponseBadRequest(
-                f"Invalid image extension. Please upload files with extensions {', '.join(self.allowed_extensions)}.")
+                f"Invalid image extension. Please upload files with extensions {', '.join(self.allowed_extensions)}."
+            )
 
         # Check the file size
         max_size_bytes = self.max_size * 1024 * 1024
         if image.size > max_size_bytes:
             raise HttpResponseBadRequest(
-                f"Image size is too large. Maximum size is {self.max_size} MB.")
+                f"Image size is too large. Maximum size is {self.max_size} MB."
+            )
 
 
 class ImageClassification:
@@ -105,7 +138,7 @@ class ImageClassification:
         Gets model prediction based on the image.
     """
 
-    def __init__(self, model: str, image_file):
+    def __init__(self, model, image_file):
         """
         Initializes the class.
 
@@ -116,8 +149,9 @@ class ImageClassification:
         self.model = model
         self.image_file = image_file
 
-
-    def process_image_raster(self, image_file: Image, target_size: Tuple[int, int] = (32, 32)) -> np.ndarray:
+    def process_image_raster(
+        self, image_file: Image, target_size: Tuple[int, int] = (32, 32)
+    ) -> np.ndarray:
         """
         Processes the uploaded raster image.
 
@@ -130,18 +164,20 @@ class ImageClassification:
         """
 
         # Check the file extension
-        if image_file.format.lower() == 'png':
+        if image_file.format.lower() == "png":
             # If the file has a png extension, convert it to RGB mode
-            image = image_file.convert('RGB')
+            image = image_file.convert("RGB")
         else:
             # Otherwise, simply open the image
-            image = image_file.convert('RGB')
+            image = image_file.convert("RGB")
 
         image = image.resize(target_size)
         image_array = np.array(image) / 255.0
         return image_array
 
-    def process_image_vector(self, vector_image: Image, target_size: Tuple[int, int] = (32, 32)) -> np.ndarray:
+    def process_image_vector(
+        self, vector_image: Image, target_size: Tuple[int, int] = (32, 32)
+    ) -> np.ndarray:
         """
         Converts vector image to a format suitable for TensorFlow model.
 
@@ -170,25 +206,40 @@ class ImageClassification:
         - prediction (dict): Dictionary with predicted image classes by the model.
         """
         prediction = model.predict(np.expand_dims(image_array, axis=0))
-        top_classes = tf.argsort(prediction, axis=1, direction='DESCENDING')[:, :3]
+        top_classes = tf.argsort(prediction, axis=1, direction="DESCENDING")[:, :3]
         probabilities = prediction[0][top_classes[0]]
         prediction_dict = {
-            'first': (CIFAR_10_LIST[top_classes[0][0]] + ': ' + str(round(probabilities[0] * 100, 2)) + '%').capitalize(),
-            'second': (CIFAR_10_LIST[top_classes[0][1]] + ': ' + str(round(probabilities[1] * 100, 2)) + '%').capitalize(),
-            'third': (CIFAR_10_LIST[top_classes[0][2]] + ': ' + str(round(probabilities[2] * 100, 2)) + '%').capitalize(),
+            "first": (
+                CIFAR_10_LIST[top_classes[0][0]]
+                + ": "
+                + str(round(probabilities[0] * 100, 2))
+                + "%"
+            ).capitalize(),
+            "second": (
+                CIFAR_10_LIST[top_classes[0][1]]
+                + ": "
+                + str(round(probabilities[1] * 100, 2))
+                + "%"
+            ).capitalize(),
+            "third": (
+                CIFAR_10_LIST[top_classes[0][2]]
+                + ": "
+                + str(round(probabilities[2] * 100, 2))
+                + "%"
+            ).capitalize(),
         }
 
         return prediction_dict
 
     def get_results(self):
-        """
-        Method to call the class as a function.
-        """
-        if self.image_file.format.lower() in ALLOWED_EXTENSION['rastr']:
+        predictions = None
+        print("Start classification")
+        print(f"Current model: {self.model.name}")
+        if self.image_file.format.lower() in ALLOWED_EXTENSION["rastr"]:
             image_array = self.process_image_raster(self.image_file)
             predictions = self.get_prediction(image_array, self.model)
-        elif self.image_file.format.lower() in ALLOWED_EXTENSION['vector']:
+        elif self.image_file.format.lower() in ALLOWED_EXTENSION["vector"]:
             image_array = self.process_image_vector(self.image_file)
             predictions = self.get_prediction(image_array, self.model)
-
+        print(f"results: {predictions}")
         return predictions
--- src/web_image_classifier/main_app/views.py
+++ src/web_image_classifier/main_app/views.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from django.shortcuts import render
 from django.http import HttpResponseBadRequest, JsonResponse
 from .tf_models.models_info import CIFAR_10_LIST, cifar_10_model
@@ -11,33 +10,35 @@ upload = UploadProcessing()
 
 def upload_image(request):
     form = UploadForm(request.POST or None, request.FILES or None)
-    if (request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest') and form.is_valid():
+    if (
+        request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
+    ) and form.is_valid():
         try:
-            upload.image = request.FILES['image']
-        except HttpResponseBadRequest as e:
-            return e
+            upload.image = request.FILES["image"]
+        except HttpResponseBadRequest as error:
+            return error
     return form
 
 
 def main(request):
     upload_form = upload_image(request)
-    context = {'upload_form': upload_form, 'classes': CIFAR_10_LIST}
+    context = {"upload_form": upload_form, "classes": CIFAR_10_LIST}
 
     return render(request, "main_app/main.html", context)
 
 
 def information(request):
     upload_form = upload_image(request)
-    context = {'upload_form': upload_form}
+    context = {"upload_form": upload_form}
 
     return render(request, "main_app/information.html", context)
 
 
 def results(request):
     if request.GET:
-        print('Start classification')
-        print(f'Current model: {cifar_10_model.name}')
-        image_classification = ImageClassification(cifar_10_model, upload.image)
-        classification_results = image_classification.get_results()
-        print(f'results: {classification_results}')
-        return JsonResponse(classification_results)
+        try:
+            image_classification = ImageClassification(cifar_10_model, upload.image)
+            classification_results = image_classification.get_results()
+            return JsonResponse(classification_results)
+        except HttpResponseBadRequest as error:
+            return error
